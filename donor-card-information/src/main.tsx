6b19d68d06e42bce6f5b791e20e40dde2d6228d2
tigerdeeda <vichayutmusic@gmail.com>
2025-03-06 22:46:52 +0700

    Edit main.tsx

--- donor-card-information/src/main.tsx
+++ donor-card-information/src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Donor_Form from './Donor_Form'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+     <Donor_Form />
   </React.StrictMode>,
 )
